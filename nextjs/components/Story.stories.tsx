61986dd49188864787c02d0942378c6c4029262c
Hiro Ari <hiro0107@users.noreply.github.com>
2023-03-01 09:25:41 +0000

    Fix Story to export as default

--- nextjs/components/Story.stories.tsx
+++ nextjs/components/Story.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Story } from './Story';
+import Story from './Story';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
--- nextjs/components/Story.tsx
+++ nextjs/components/Story.tsx
@@ -2,7 +2,7 @@ type Props = {
   text: string
 }
 
-export function Story({text}: Props) {
+export default function Story({text}: Props) {
   return (<>
     <div className={"bg-yellow-100 p-[5px] w-[150px] h-[70px] text-[16px] drop-shadow-lg overflow-y-auto"}>
       {
